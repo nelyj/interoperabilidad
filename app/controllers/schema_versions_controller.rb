d8125be831d23933507d703e75e8373b0669aaf6
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-06-06 17:18:40 -0400

    Add logic to schema/schema_version controllers

--- app/controllers/schema_versions_controller.rb
+++ app/controllers/schema_versions_controller.rb
@@ -1,2 +1,43 @@
 class SchemaVersionsController < ApplicationController
+
+  def show
+    @schema_version = SchemaVersion.find(params[:version_number])
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def index
+    set_schema
+    @schema_versions = @schema.schema_versions
+  end
+
+  def new
+    @schema_version = SchemaVersion.new
+    set_schema
+  end
+
+  def create
+    set_schema
+    @schema_version = @schema.schema_versions.build(schema_version_params)
+    if @schema_version.save
+      redirect_to [@schema, @schema_version], notice: 'schema_version was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_schema_version
+    @schema_version = SchemaVersion.find(params[:id])
+  end
+
+  def set_schema
+    @schema = Schema.find_by(name: params[:schema_name])
+  end
+
+  def schema_version_params
+    params.require(:schema_version).permit(:spec)
+  end
 end
--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -1,3 +1,46 @@
 class SchemasController < ApplicationController
 
+  def index
+    @schemas = Schema.all
+    set_categories
+  end
+
+  def new
+    @schema = Schema.new
+    set_categories
+  end
+
+  def create
+    @schema = Schema.new(params.require(:schema).permit(:schema_category_id, :name, :spec))
+    if @schema.save
+      redirect_to [@schema, @schema.schema_versions.first], notice: 'schema was successfully created.'
+    else
+      flash.now[:error] = "Could not save schema"
+      render action: "new"
+    end
+  end
+
+  def edit
+    @schema = Schema.find_by(name: params[:name])
+    set_categories
+  end
+
+  def update
+    @schema = Schema.find_by(name: params[:name])
+    if @schema.update(params.require(:schema).permit(:schema_category_id))
+      redirect_to schemas_path, notice: 'schema was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def set_schema
+      @schema = Schema.find(params[:id])
+    end
+
+    def set_categories
+      @categories = SchemaCategory.all.map { |category| [category.name, category.id] }
+    end
 end
