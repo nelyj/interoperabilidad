fd565517d56bf3f49c72f0235732740a340e7357
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-06-06 16:18:42 -0400

    Add some logic to schema models
    Set automatically version number on schema_version. Modify method to_param because it should return a string.
    Validate uniqueness of name and change create_first_version method.

--- app/models/schema.rb
+++ app/models/schema.rb
@@ -2,7 +2,17 @@ class Schema < ApplicationRecord
   belongs_to :schema_category
   has_many :schema_versions
 
+  validates :name, uniqueness: true
+
+  after_create :create_first_version
+
+  attr_accessor :spec
+
   def to_param
     name
+  end
+
+  def create_first_version
+    schema_versions.create(spec: self.spec)
   end
 end
--- app/models/schema_version.rb
+++ app/models/schema_version.rb
@@ -1,8 +1,14 @@
 class SchemaVersion < ApplicationRecord
   belongs_to :schema
   validates :spec, swagger_schema_object: true
+  before_create :set_version_number
 
   def to_param
-    version_number
+    version_number.to_s
+  end
+
+  def set_version_number
+    last_version = schema.schema_versions.maximum(:version_number) || 0
+    self.version_number = last_version.to_i + 1
   end
 end
