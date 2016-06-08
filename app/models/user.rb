96cd29718269508692557a71e4fff915cc223f1c
AFiebig <alfredo.fiebig@continuum.cl>
2016-06-08 12:50:16 -0400

    Applies changes suggested by @leosoto
    Also, fix an error saving the Roles and adds email to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,20 +8,22 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     puts "#{auth.inspect}"
-    where(rut: auth.info.rut).first_or_create! do |user|
+    new_user = where(rut: auth.info.rut).first_or_create! do |user|
       user.sub = auth.info.sub
       user.id_token = auth.info.id_token
-      add_role_from_service!(user)
     end
+    add_role_from_service!(new_user)
+    new_user
   end
 
-  def add_role_from_service!(user)
+  # TODO: need to be changed later, to use the provided service to get the roles.
+  def self.add_role_from_service!(user)
     user.can_create_schemas = true
+    user.email = "mail@example.org"
+    # Organization has to be updated if it changes
     org = Organization.find_by_initials("SEGPRES")
-    rol = Role.first_or_create(user_id: user.id,
-                               organization_id: org.id,
-                               name: "Schema Admin")
-    user.save
+    user.roles.first_or_create!(organization: org, name: "Schema Admin")
+    user.save!
   end
 
 end
