12dbe05ceb2a9faf08172a0f6046882dcb05778a
Leonardo Soto M <leo.soto@gmail.com>
2016-06-08 17:35:01 -0400

    WIP: Model test for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,23 +6,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :trackable, :omniauthable, omniauth_providers: [:clave_unica]
 
-  def self.from_omniauth(auth)    
+  def self.from_omniauth(auth)
     new_user = where(rut: auth.info.rut).first_or_create! do |user|
       user.sub = auth.info.sub
       user.id_token = auth.info.id_token
     end
-    add_role_from_service!(new_user)
-    new_user
+    new_user.add_role_from_service!
   end
 
   # TODO: need to be changed later, to use the provided service to get the roles.
-  def self.add_role_from_service!(user)
-    user.can_create_schemas = true
-    user.email = "mail@example.org"
+  def refresh_user_roles_and_email!
+    self.can_create_schemas = true
+    self.email = "mail@example.org"
     # Organization has to be updated if it changes
     org = Organization.find_by_initials("SEGPRES")
-    user.roles.first_or_create!(organization: org, name: "Schema Admin")
-    user.save!
+    self.roles.first_or_create!(organization: org, name: "Schema Admin")
+    save!
   end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#refresh_user_roles_and_email! sets a hard coded email, org, flags and roles' do
+    user = users(:perico)
+    segpres = organizations(:segpres)
+    user.refresh_user_roles_and_email!
+    assert_equal "mail@example.org", user.email
+    assert_equal segpres, user.organization
+    assert user.can_create_schemas
+    assert "Schema Admin", user.roles.where(organization: segpres).first.name
+  end
 end
