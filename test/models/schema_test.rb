6e8dedc2e6e371353f646c50e535889a2aa21229
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-06-09 11:17:48 -0400

    Fix tests for schema{,_version}

--- test/models/schema_test.rb
+++ test/models/schema_test.rb
@@ -6,13 +6,13 @@ class SchemaTest < ActiveSupport::TestCase
     schema = Schema.create(
       schema_category: schema_categories(:informacion_de_personas),
       name: 'test-schema',
-      spec: VALID_SCHEMA_OBJECT
+      spec_file: StringIO.new(VALID_SCHEMA_OBJECT)
     )
     assert_equal 1, schema.last_version_number
     # VALID_SCHEMA_OBJECT is loaded by test_helper
-    schema.schema_versions.create(spec: VALID_SCHEMA_OBJECT)
+    schema.schema_versions.create(spec_file: StringIO.new(VALID_SCHEMA_OBJECT))
     assert_equal 2, schema.last_version_number
-    schema.schema_versions.create(spec: VALID_SCHEMA_OBJECT)
+    schema.schema_versions.create(spec_file: StringIO.new(VALID_SCHEMA_OBJECT))
     assert_equal 3, schema.last_version_number
   end
 end
--- test/models/schema_version_test.rb
+++ test/models/schema_version_test.rb
@@ -4,9 +4,9 @@ class SchemaVersionTest < ActiveSupport::TestCase
   test 'validations' do
     # VALID_SCHEMA_OBJECT and INVALID_SCHEMA_OBJECT are loaded by test_helper
     valid_schema_version = SchemaVersion.new(
-      spec: VALID_SCHEMA_OBJECT, schema: schemas(:rut))
+      spec_file: StringIO.new(VALID_SCHEMA_OBJECT), schema: schemas(:rut))
     invalid_schema_version = SchemaVersion.new(
-      spec: INVALID_SCHEMA_OBJECT, schema: schemas(:rut))
+      spec_file: StringIO.new(INVALID_SCHEMA_OBJECT), schema: schemas(:rut))
     assert valid_schema_version.valid?
     assert_not invalid_schema_version.valid?
     assert_not invalid_schema_version.errors[:spec].blank?
