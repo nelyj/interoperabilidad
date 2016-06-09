71ab9fa2dd743624c713282258bf922a196a5505
AFiebig <alfredo.fiebig@continuum.cl>
2016-06-09 16:15:45 -0400

    Add test for .from_omniauth when a user already exists

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
+
 class UserTest < ActiveSupport::TestCase
+
+  module LookLikeJSON
+    def method_missing(meth, *args, &block)
+      if has_key?(meth.to_s)
+        self[meth.to_s]
+      else
+        raise NoMethodError, 'undefined method #{meth} for #{self}'
+      end
+    end
+  end
+
   test '#refresh_user_roles_and_email! sets a hard coded email, org, flags and roles' do
     user = users(:perico)
     segpres = organizations(:segpres)
@@ -12,16 +24,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test '.from_omniauth set Auth params to new user' do
-    module LookLikeJSON
-      def method_missing(meth, *args, &block)
-        if has_key?(meth.to_s)
-          self[meth.to_s]
-        else
-          raise NoMethodError, 'undefined method #{meth} for #{self}'
-        end
-      end
-    end
-
     info = { "rut" => "22.222.222-2", "sub" => "8", "id_token" => "ASDF" }
     info.extend(LookLikeJSON)
     auth = {"info" => info}
@@ -32,6 +34,7 @@ class UserTest < ActiveSupport::TestCase
     segpres = organizations(:segpres)
 
     assert_equal "22.222.222-2", user.rut
+    assert_equal "Perico", user.name
     assert_equal "8", user.sub
     assert_equal "ASDF", user.id_token
     assert_equal "mail@example.org", user.email
@@ -39,4 +42,31 @@ class UserTest < ActiveSupport::TestCase
     assert user.can_create_schemas
     assert "Schema Admin", user.roles.where(organization: segpres).first.name
   end
+
+  test '.from_omniauth set Auth params to existing user' do
+    info = { "rut" => "11.111.111-1", "sub" => "8", "id_token" => "ASDF" }
+    info.extend(LookLikeJSON)
+    auth = {"info" => info}
+    auth.extend(LookLikeJSON)
+
+    user = User.where(rut: auth.info.rut).first
+    assert_equal "11.111.111-1", user.rut
+    assert_equal "2", user.sub
+    assert_equal "some-token", user.id_token
+
+    User.from_omniauth(auth)
+
+    user = User.where(rut: auth.info.rut).first
+    segpres = organizations(:segpres)
+
+    assert_equal "11.111.111-1", user.rut
+    assert_equal "Perico", user.name
+    assert_equal "8", user.sub
+    assert_equal "ASDF", user.id_token
+    assert_equal "mail@example.org", user.email
+    assert_equal segpres, user.organizations.first
+    assert user.can_create_schemas
+    assert "Schema Admin", user.roles.where(organization: segpres).first.name
+  end
+
 end
