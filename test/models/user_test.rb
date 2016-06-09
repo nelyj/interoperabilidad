272826c30da3cd75782cc0212b3b368409ea738f
AFiebig <alfredo.fiebig@continuum.cl>
2016-06-09 12:26:34 -0400

    Add test for .from_omniauth
    Also, fix test for refresh_user_roles_and_email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,36 @@ class UserTest < ActiveSupport::TestCase
     segpres = organizations(:segpres)
     user.refresh_user_roles_and_email!
     assert_equal "mail@example.org", user.email
-    assert_equal segpres, user.organization
+    assert_equal segpres, user.organizations.first
+    assert user.can_create_schemas
+    assert "Schema Admin", user.roles.where(organization: segpres).first.name
+  end
+
+  test '.from_omniauth set Auth params to new user' do
+    module LookLikeJSON
+      def method_missing(meth, *args, &block)
+        if has_key?(meth.to_s)
+          self[meth.to_s]
+        else
+          raise NoMethodError, 'undefined method #{meth} for #{self}'
+        end
+      end
+    end
+
+    info = { "rut" => "22.222.222-2", "sub" => "8", "id_token" => "ASDF" }
+    info.extend(LookLikeJSON)
+    auth = {"info" => info}
+    auth.extend(LookLikeJSON)
+
+    User.from_omniauth(auth)
+    user = User.where(rut: auth.info.rut).first
+    segpres = organizations(:segpres)
+
+    assert_equal "22.222.222-2", user.rut
+    assert_equal "8", user.sub
+    assert_equal "ASDF", user.id_token
+    assert_equal "mail@example.org", user.email
+    assert_equal segpres, user.organizations.first
     assert user.can_create_schemas
     assert "Schema Admin", user.roles.where(organization: segpres).first.name
   end
