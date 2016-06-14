db57141d4c3ceaa94522e4983bbd810785d1e5e3
Nicolás Silva <sovietfrance@gmail.com>
2016-06-14 11:22:13 -0400

    Add 'fonts' folder to config.assets.paths

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module App
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W["#{config.root}/app/validators/"]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
