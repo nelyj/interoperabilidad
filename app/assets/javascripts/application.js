7b594829c676766684465ba2f247b05621263404
Nicolás Silva <sovietfrance@gmail.com>
2016-06-15 15:51:15 -0400

    Add 'Select2' support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,10 @@
-//= require jquery
-//= require bootstrap
-//= require turbolinks
+// = require jquery
+// = require bootstrap
+// = require turbolinks
+// = require select2.full
+$(document).ready(function() {
+    $("select").select2({
+        theme: "bootstrap",
+        containerCssClass: ":all:"
+    });
+});
