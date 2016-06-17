c92ea70d4c187a69dd5cdbb982e3af76a5155f98
AFiebig <alfredo.fiebig@continuum.cl>
2016-06-17 16:19:45 -0400

    Add test for ServiceVersion#retract_proposed

--- test/models/service_version_test.rb
+++ test/models/service_version_test.rb
@@ -12,4 +12,70 @@ class ServiceVersionTest < ActiveSupport::TestCase
     assert_not invalid_service_version.valid?
     assert_not invalid_service_version.errors[:spec].blank?
   end
+
+  test '#retract_proposed create 3 new versions for a '\
+       'Service and only the last one have to be proposed.' do
+    service = services(:servicio_1)
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                    user: users(:perico))
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+    assert_equal 3, service.service_versions.length
+    assert_equal 2, service.service_versions.retracted.length
+    assert_equal 1, service.service_versions.proposed.length
+  end
+
+  test '#retract_proposed create new versions for a '\
+       'Service and only the 3 proposed shuld be modified.' do
+    service = services(:servicio_1)
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                    user: users(:perico))
+    service.service_versions.last.rejected!
+
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+    service.service_versions.last.retired!
+
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+    service.service_versions.last.outdated!
+
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+    service.service_versions.last.current!
+
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+
+    service.service_versions.create!(spec_file: StringIO.new(VALID_SPEC),
+                                     service: service,
+                                     user: users(:perico))
+
+    puts service.service_versions.length
+    puts service.service_versions.rejected.length
+    puts service.service_versions.retired.length
+    puts service.service_versions.outdated.length
+    puts service.service_versions.current.length
+    puts service.service_versions.retracted.length
+    puts service.service_versions.proposed.length
+
+    assert_equal 6, service.service_versions.length
+    assert_equal 1, service.service_versions.rejected.length
+    assert_equal 1, service.service_versions.retired.length
+    assert_equal 1, service.service_versions.outdated.length
+    assert_equal 1, service.service_versions.current.length
+    assert_equal 1, service.service_versions.retracted.length
+    assert_equal 1, service.service_versions.proposed.length
+  end
+
 end
