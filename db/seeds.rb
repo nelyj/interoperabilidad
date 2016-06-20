842a6d46cd5f1b38cf5ca52320152f8ef7ef837a
Nicolás Silva <sovietfrance@gmail.com>
2016-06-20 12:39:09 -0400

    Add description to schema object

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 VALID_SCHEMA_OBJECT = '{
   "type": "object",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   "required": [
     "name"
   ],
