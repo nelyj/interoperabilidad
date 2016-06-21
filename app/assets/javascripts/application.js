6ded83c7c9cd0295febdeba761f8dd16896a973c
Nicolás Silva <sovietfrance@gmail.com>
2016-06-21 11:10:43 -0400

    Update application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,6 @@
-// = require jquery
-// = require jquery_ujs
-// = require bootstrap
-// = require turbolinks
-// = require select2.full
-$(document).ready(function() {
-    $("select").select2({
-        theme: "bootstrap",
-        containerCssClass: ":all:"
-    });
-});
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require select2.full
+//= require_tree .
