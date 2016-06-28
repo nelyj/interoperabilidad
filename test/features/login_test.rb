45781dd9dce67a02d06831d428167c239b7e7351
Leonardo Soto M <leo.soto@gmail.com>
2016-06-28 16:38:46 -0400

    Change login feature test to use the declarative style

--- test/features/login_test.rb
+++ test/features/login_test.rb
@@ -2,10 +2,9 @@ require "test_helper"
 
 class LoginTest < Capybara::Rails::TestCase
   include Warden::Test::Helpers
-
   after { Warden.test_reset! }
 
-  def test_login
+  test "login" do
     visit root_path
     assert page.has_css?('#btn-login')
     login_as users(:perico), scope: :user
