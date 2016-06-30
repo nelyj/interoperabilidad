dba75d794514f9af0d0d320db63fa73b18d6e76e
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-06-30 11:58:17 -0400

    Small refactor on schema_search_test

--- test/features/schema_search_test.rb
+++ test/features/schema_search_test.rb
@@ -6,22 +6,22 @@ class SchemaSearchTest < Capybara::Rails::TestCase
 
   test "succesful search using schema name" do
     visit schemas_path
-    fill_in('search-schema', :with => "rut")
-    find_by_id('search-schema').send_keys(:enter)
+    fill_in 'search-schema', :with => "rut"
+    find_by_id('search-schema').send_keys :enter
     assert_content page, "RUT de una persona o empresa"
   end
 
   test "succesful search using schema description" do
     visit schemas_path
-    fill_in('search-schema', :with => "empresa")
+    fill_in 'search-schema', :with => "empresa"
     find_by_id('search-schema').send_keys(:enter)
     assert_content page, "RUT de una persona o empresa"
   end
 
   test "search results not found" do
     visit schemas_path
-    fill_in('search-schema', :with => "patente")
-    find_by_id('search-schema').send_keys(:enter)
+    fill_in 'search-schema', :with => "patente"
+    find_by_id('search-schema').send_keys :enter
     assert_content page, "No se han encontrado resultados"
   end
 end
