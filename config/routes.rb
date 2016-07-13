493326793a42ff966b5cc5f3cb47e3c13cf48a57
Ricardo Alfaro <ricardoalfarog@gmail.com>
2016-07-13 19:44:29 -0400

    add views for agreements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/visual_components'
+  get 'agreements/index'
+  get 'agreements/show'
+  get 'agreements/new'
+  get 'agreements/edit'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
