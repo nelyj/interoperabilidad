594476b26bf0611bd8068437cd5f6cd12936e987
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-07-18 14:39:31 -0400

    Add method to map css_class to service_version helper

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,2 +1,13 @@
 module ServicesHelper
+
+  def css_class_for_status(status)
+    {
+      'outdated' => 'static',
+      'retired' => 'default',
+      'proposed' => 'primary',
+      'current' => 'success',
+      'retracted' => 'warning',
+      'rejected' => 'danger'
+    }[status] + ' btn-status' || ''
+  end
 end
