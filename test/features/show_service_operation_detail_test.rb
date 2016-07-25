35572e6207f4bcd675ef38a564d60c4014375f5c
Leonardo Soto M <leo.soto@gmail.com>
2016-07-25 16:10:08 -0400

    Add test to check we show the JSON inputs on the console

--- test/features/show_service_operation_detail_test.rb
+++ test/features/show_service_operation_detail_test.rb
@@ -40,6 +40,23 @@ class ShowServiceOperationDetailTest < Capybara::Rails::TestCase
     end
   end
 
+  test "Show test service form with raw JSON input for users allowed to use the service" do
+    login_as users(:pedro)
+    visit organization_service_service_version_path(
+      @service_v.organization, @service_v.service, @service_v
+    )
+    find(".container-verbs a", text: "GET/users/login").click
+    assert_content "Logs user into the system"
+    click_button "Probar Servicio"
+    within ".console" do
+      assert_content "Parámetros"
+      assert_content "URL: Query"
+      fill_in "username", with: "Something"
+      find("a", text: "JSON").click
+      assert_content '"username": "Something"'
+    end
+  end
+
   test "Don't show test service form for users not allowed to use the service" do
     login_as users(:pablito)
     visit organization_service_service_version_path(
