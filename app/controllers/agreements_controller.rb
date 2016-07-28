4979d9781b95d810a342fd3607dbd1bfd23683f1
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-07-28 13:28:59 -0400

    Add create action to agreements_controller

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -2,24 +2,37 @@ class AgreementsController < ApplicationController
   before_action :set_organization
 
   def index
-    
+    @agreements = Agreement.all
   end
 
   def show
   end
 
   def new
+    @agreement = Agreement.new
+  end
+
+  def create
+    @agreement = Agreement.new(agreement_params)
+    if @agreement.save
+      redirect_to(agreements_path)
+    else
+      flash.now[:error] = t(:cant_create_service)
+      render action: "new"
+    end
   end
 
   def edit
   end
 
-  private
-    def service_params
-      params.require(:service).permit(:organization_id, :name, :spec_file, :public, :backwards_compatible)
-    end
+private
 
-    def set_organization
+  def agreement_params
+    params.require(:agreement).permit(:service_provider_organization_id,
+      :service_consumer_organization_id)
+  end
+
+  def set_organization
       @organization = Organization.where(name: params[:organization_name]).first
-    end
+  end
 end
