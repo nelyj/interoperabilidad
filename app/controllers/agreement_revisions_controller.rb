7d1409daa0f25621dc42a798fad3e4dcd9a2cfaa
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-08-04 17:10:55 -0400

    Add some logic to agreement_revisions controller

--- app/controllers/agreement_revisions_controller.rb
+++ app/controllers/agreement_revisions_controller.rb
@@ -4,6 +4,10 @@ class AgreementRevisionsController < ApplicationController
   end
 
   def show
+    @agreement = Agreement.find(params[:agreement_id])
+    @agreement_revision = @agreement.agreement_revisions.where(revision_number: params[:revision_number]).first
+    @provider_organization = Organization.find(@agreement.service_provider_organization_id)
+    @consumer_organization = Organization.find(@agreement.service_consumer_organization_id)
     respond_to do |format|
       format.html do
       end
