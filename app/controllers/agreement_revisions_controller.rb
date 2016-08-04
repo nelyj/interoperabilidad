fc145270728389f885a367f8c481835324dd60a6
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-08-04 11:11:13 -0400

    Add agreement_revisions{_controller, views}

---
+++ app/controllers/agreement_revisions_controller.rb
@@ -0,0 +1,21 @@
+class AgreementRevisionsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    respond_to do |format|
+      format.html do
+      end
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+end
