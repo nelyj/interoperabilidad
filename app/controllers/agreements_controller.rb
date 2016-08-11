825dce9952d349eb06a81475fe1cea7099a75ed4
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-08-11 10:43:58 -0400

    Add PDF generation to agreements

--- app/controllers/agreements_controller.rb
+++ app/controllers/agreements_controller.rb
@@ -15,8 +15,8 @@ class AgreementsController < ApplicationController
 
   def create
     @agreement = Agreement.new(agreement_params.merge(user: current_user))
-
-     if @agreement.save!
+    if @agreement.save!
+      generate_pdf(@agreement)
       redirect_to(
         organization_agreement_agreement_revision_path(@organization, @agreement,  @agreement.agreement_revisions.last.revision_number),
         notice: t(:new_agreement_created)
@@ -31,6 +31,21 @@ class AgreementsController < ApplicationController
   end
 
 private
+
+  def generate_pdf(agreement)
+    @agreement_revision = agreement.agreement_revisions.first
+    @provider_organization = Organization.find(agreement.service_provider_organization_id)
+    @consumer_organization = Organization.find(agreement.service_consumer_organization_id)
+    file_name = "CID-#{agreement.id}-#{@agreement_revision.revision_number}_#{@provider_organization.initials.upcase}_#{@consumer_organization.initials.upcase}.pdf"
+    file_path = Rails.root.join('tmp', file_name)
+    render  :pdf => 'convenio',
+            :template => 'agreement_revisions/template.html.haml',
+            :assigns => { agreement: agreement },
+            :save_to_file => file_path,
+            :save_only => true
+    @agreement_revision.upload_pdf(file_name, file_path)
+    @agreement_revision.save
+  end
 
   def agreement_params
     params.require(:agreement).permit(:service_provider_organization_id,
--- app/models/agreement_revision.rb
+++ app/models/agreement_revision.rb
@@ -1,4 +1,5 @@
 class AgreementRevision <ApplicationRecord
+  include S3Configuration
   belongs_to :agreement
   belongs_to :user
   before_create :set_revision_number
@@ -9,4 +10,12 @@ class AgreementRevision <ApplicationRecord
     self.revision_number = agreement.last_revision_number + 1
   end
 
+  def upload_pdf(file_name, file_path)
+    new_object = codegen_bucket.objects.build(file_name)
+    new_object.content = open(file_path)
+    new_object.acl = :public_read
+    new_object.content_type = 'application/pdf'
+    new_object.save
+    self.file = new_object.url
+  end
 end
