b2ef2684086a1dad7ac976ae511a45c572e88314
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-08-11 17:15:21 -0400

    Add events history for agreement_revisions#show

--- app/controllers/agreement_revisions_controller.rb
+++ app/controllers/agreement_revisions_controller.rb
@@ -8,15 +8,6 @@ class AgreementRevisionsController < ApplicationController
     @agreement_revision = @agreement.agreement_revisions.where(revision_number: params[:revision_number]).first
     @provider_organization = Organization.find(@agreement.service_provider_organization_id)
     @consumer_organization = Organization.find(@agreement.service_consumer_organization_id)
-    respond_to do |format|
-      format.html
-      format.pdf do
-        render  :pdf => 'report',
-                :template => 'agreement_revisions/template.html.haml',
-                :assigns => { agreement: @agreement },
-                :save_to_file => Rails.root.join('tmp', "#{@agreement.id}.pdf")
-      end
-    end
   end
 
   def new
