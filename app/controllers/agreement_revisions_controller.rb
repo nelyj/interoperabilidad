c192b24c04d7c10a9753b3acfe8d13c643d41f9c
Carlos Fiebig <carlos.fiebig@continuum.cl>
2016-08-29 16:18:29 -0300

    Add validations & translations for agreement actions

--- app/controllers/agreement_revisions_controller.rb
+++ app/controllers/agreement_revisions_controller.rb
@@ -30,30 +30,60 @@ class AgreementRevisionsController < ApplicationController
 
   def validation_request
     @agreement_revision = @agreement.validate_draft(current_user)
-    generate_pdf(@agreement, @agreement_revision)
-    redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_sent)
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:agreement_wrongly_sent)
+    else
+      generate_pdf(@agreement, @agreement_revision)
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_sent)
+    end
   end
 
   def consumer_signature
     @agreement_revision = @agreement.sign_draft(current_user)
-    generate_pdf(@agreement, @agreement_revision)
-    redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_signed)
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:agreement_wrongly_signed)
+    else
+      generate_pdf(@agreement, @agreement_revision)
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_signed)
+    end
   end
 
   def objection_request
     @agreement_revision = @agreement.object_draft(current_user, agreement_revision_params[:objection_message])
-    generate_pdf(@agreement, @agreement_revision)
-    redirect_to [@organization, @agreement, @agreement_revision], notice: t(:objection_correctly_sent)
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:objection_wrongly_sent)
+    else
+      generate_pdf(@agreement, @agreement_revision)
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:objection_correctly_sent)
+    end
   end
 
   def document_validation
     @agreement_revision = @agreement.validate_revision(current_user)
-    redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_sent)
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:agreement_wrongly_sent)
+    else
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_sent)
+    end
   end
 
   def provider_signature
     @agreement_revision = @agreement.sign(current_user)
-    redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_signed)
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:agreement_wrongly_signed)
+    else
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:agreement_correctly_signed)
+    end
+  end
+
+  def reject_signature
+    @agreement_revision = @agreement.reject_sign(current_user, agreement_revision_params[:objection_message])
+    if @agreement_revision.nil?
+      flash.now[:error] = t(:rejection_wrongly_sent)
+    else
+      generate_pdf(@agreement, @agreement_revision)
+      redirect_to [@organization, @agreement, @agreement_revision], notice: t(:rejection_correctly_sent)
+    end
   end
 
 private
