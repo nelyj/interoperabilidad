bd5eb0b2f49c2a97db62fefed1e14d69a6744f21
Leonardo Soto M <leo.soto@gmail.com>
2017-10-31 21:46:00 -0300

    Remove unused and useless test.
    ServiceVersionHealthCheck is just a container for the result of a
    check. The logic lives (for now) in ServiceVersion

--- test/models/service_version_health_check_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe ServiceVersionHealthCheck do
-  let(:service_version_health_check) { ServiceVersionHealthCheck.new }
-
-  it "must be valid" do
-    value(service_version_health_check).must_be :valid?
-  end
-end
