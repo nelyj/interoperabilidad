d23742de1b28612b6954b16c75a782e7bed4e4cf
Luis Hernandez <luelher@gmail.com>
2017-11-06 12:32:42 -0300

    created complex example yaml file and tests

---
+++ test/features/complex_example_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+require 'yaml'
+
+class ComplexExampleTest < Capybara::Rails::TestCase
+  include Warden::Test::Helpers
+  after { Warden.test_reset! }
+
+  test "complex service" do
+    swagger = YAML.load_file("#{Rails.root}/test/files/sample-services/ComplexExample.yaml")
+    url_complex_example = swagger['host']+swagger['basePath']+swagger['paths'].keys.first
+    visit "#{swagger['schemes'].first}://#{url_complex_example}"
+    assert_content 'pedro@dominio.com'
+    assert_content 'Juan Andres'
+    assert_content 'Perez Cortez'    
+  end
+
+end
