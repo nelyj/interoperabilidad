8af9be6c7878e62161809474677f3adec47b5be5
afiebig <alfredo.fiebig@continuum.cl>
2017-11-22 23:46:36 -0300

    Secure Organization ID
    Now, only New/Create pass the org_id as parameter, so is not posible to update te organization when editing.
    Also, add a minimum of 1 to the Threshold and Frecuency parameters.

--- app/controllers/monitor_params_controller.rb
+++ app/controllers/monitor_params_controller.rb
@@ -14,7 +14,7 @@ class MonitorParamsController < ApplicationController
   end
 
   def create
-    @monitor_param = MonitorParam.new(monitor_param_params)
+    @monitor_param = MonitorParam.new(monitor_param_params_new)
     if @monitor_param.save
       ServiceVersion.joins(:service).where(services: {organization_id: @monitor_param.organization_id}).current.each(&:reschedule_health_checks)
       redirect_to monitor_params_path, notice: t(:new_monitor_param_created)
@@ -44,8 +44,12 @@ class MonitorParamsController < ApplicationController
       @monitor_param = MonitorParam.find(params[:id])
     end
 
-    def monitor_param_params
+    def monitor_param_params_new
       params.require(:monitor_param).permit(:health_check_frequency, :unavailable_threshold, :organization_id)
+    end
+
+    def monitor_param_params
+      params.require(:monitor_param).permit(:health_check_frequency, :unavailable_threshold)
     end
 
     def set_organizations
