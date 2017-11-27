96f55c7146fd770d7a6962c707b5d936eb715482
afiebig <alfredo.fiebig@continuum.cl>
2017-11-27 11:03:15 -0300

    Refactor multi-line unless to normal unless

--- app/controllers/monitor_params_controller.rb
+++ app/controllers/monitor_params_controller.rb
@@ -55,8 +55,9 @@ class MonitorParamsController < ApplicationController
     end
 
     def check_service_admin
-      redirect_to services_path, notice: t(:cant_manage_monitor_params) unless
-        user_signed_in? && current_user.is_service_admin?
+      unless user_signed_in? && current_user.is_service_admin?
+        redirect_to services_path, notice: t(:cant_manage_monitor_params)
+      end
     end
 
 end
