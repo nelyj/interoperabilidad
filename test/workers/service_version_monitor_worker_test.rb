ddde55533007428cf531b85393899aa6b7e09366
Leonardo Soto M <leo.soto@gmail.com>
2017-12-11 09:43:33 -0300

    Add remaining uncovered case in monitor worker test

--- test/workers/service_version_monitor_worker_test.rb
+++ test/workers/service_version_monitor_worker_test.rb
@@ -17,4 +17,16 @@ class ServiceVersionMonitorWorkerTest < Minitest::Test
     ServiceVersionMonitorWorker.new.perform(-1)
     assert true # Nothing raised
   end
+
+  def test_monitor_non_current_service_version
+    service_version_mock = Minitest::Mock.new
+    service_version_mock.expect :current?, false
+    ServiceVersion.stub :find, -> (id) {
+      assert_equal 42,  id
+      service_version_mock
+    } do
+      ServiceVersionMonitorWorker.new.perform(42)
+    end
+    assert_mock service_version_mock
+  end
 end
