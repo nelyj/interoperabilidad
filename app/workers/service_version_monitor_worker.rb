af6d166eba517882a38662132de154943ec47d8c
Leonardo Soto M <leo.soto@gmail.com>
2017-12-13 18:01:01 -0300

    Shortcircuit conditionals to recude cognitive complexity reported by CC

--- app/workers/service_version_monitor_worker.rb
+++ app/workers/service_version_monitor_worker.rb
@@ -3,15 +3,15 @@ class ServiceVersionMonitorWorker
 
   def perform(service_version_id)
     service_version = ServiceVersion.find(service_version_id)
-    if service_version.current?
-      if service_version.monitoring_enabled?
-        service_version.perform_health_check!
-      else
-        Rails.logger.warn("ServiceVersionMonitorWorker: Service version #{service_version_id}} has monitoring disabled")
-      end
-    else
+    unless service_version.current?
       Rails.logger.warn("ServiceVersionMonitorWorker: Service version #{service_version_id} not current and shouldn't be monitored")
+      return
     end
+    unless service_version.monitoring_enabled?
+      Rails.logger.warn("ServiceVersionMonitorWorker: Service version #{service_version_id}} has monitoring disabled")
+      return
+    end
+    service_version.perform_health_check!
   rescue ActiveRecord::RecordNotFound
     Rails.logger.warn("ServiceVersionMonitorWorker: Service version #{service_version_id} not found")
     return
