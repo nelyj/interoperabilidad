996f5b4ab67ec0a7b1bee918612d68ebf912cb5b
Eugenio <eugenionlopez@gmail.com>
2017-12-20 18:37:54 -0300

    Remove empty rspec style test template from ServiceDataCategoryTest - Remove empty rspec test template - Test left empty as the model currently has no logic in it

--- test/models/service_data_category_test.rb
+++ test/models/service_data_category_test.rb
@@ -1,9 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
-describe ServiceDataCategory do
-  let(:service_data_category) { ServiceDataCategory.new }
-
-  it "must be valid" do
-    value(service_data_category).must_be :valid?
-  end
+class ServiceDataCategoryTest < ActiveSupport::TestCase
 end
