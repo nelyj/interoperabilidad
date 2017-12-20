702d2f521c0182ef61f622d82380317ebdf53797
Eugenio <eugenionlopez@gmail.com>
2017-12-20 17:26:04 -0300

    Add data categories - Add CRUD for data_categories - Add tests - Add filtering data categories on DataCategoriesController#index

--- app/controllers/data_categories_controller.rb
+++ app/controllers/data_categories_controller.rb
@@ -3,16 +3,48 @@ class DataCategoriesController < ApplicationController
   def index
   end
 
-  def show
+  def new
+    @data_category = DataCategory.new
   end
 
-  def create
+  def create  
+    @data_category = DataCategory.new(data_category_params)
+    if @data_category.save
+      redirect_to data_categories_path, notice: t(:record_created)
+    else
+      flash.now[:error] = t(:couldnt_save_record)
+      redirect_to data_categories_path
+    end
+  end
+
+  def edit
+    @data_category = DataCategory.find(params[:id])
   end
 
   def update
+    @data_category = DataCategory.find(params[:id])
+    if @data_category.update!(data_category_params)
+      flash[:success] = I18n.t(:data_category_update_success)
+      redirect_to data_categories_path
+    else
+      flash[:error] = I18n.t(:data_category_update_error)
+      redirect_to data_categories_path
+    end
   end
 
   def destroy
+    if DataCategory.find(params[:id]).destroy
+      flash[:success] = I18n.t(:data_category_deletion_success)
+      redirect_to data_categories_path
+    else
+      flash[:error] = I18n.t(:data_category_deletion_error)
+      redirect_to data_categories_path
+    end
+  end
+
+  private
+  def data_category_params
+    params.require(:data_category).permit(:name)
   end
 
 end
--- test/controllers/data_categories_controller_test.rb
+++ test/controllers/data_categories_controller_test.rb
@@ -1,7 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
-describe DataCategoriesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+class DataCategoriesControllerTest < ActionDispatch::IntegrationTest
+
+  test 'should render index' do
+    get data_categories_path
+    assert_equal response.status, 200
+  end
+
+  test 'should create a new data category' do
+    assert_difference 'DataCategory.count', 1 do
+      post data_categories_path, params: { data_category: { name: 'this is a tag' }}
+    end
+    assert DataCategory.find_by(name: 'this is a tag')
+  end
+
+  test 'should modify a data category' do
+    to_be_modified_id = DataCategory.all.first.id
+    patch data_category_path(id: to_be_modified_id, data_category: { name: 'whatever else'})
+    assert_equal response.status, 302 # Redirected to index
+    assert_equal DataCategory.find(to_be_modified_id).name, 'whatever else'
+  end
+
+  test 'should delete a data category' do
+    assert_difference 'DataCategory.count', -1 do
+      to_be_deleted_id = DataCategory.all.first.id
+      delete data_category_path({ id: to_be_deleted_id })
+    end
+    assert_equal response.status, 302 # Redirected to index
+  end
 end
