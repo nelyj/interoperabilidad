4946eb7f14eac38da0fe9e72331677e0a3f3a8e5
Eugenio <eugenionlopez@gmail.com>
2017-12-26 12:17:53 -0300

    Delete related {service,schema}_data_categories on data_category delete

--- app/models/data_category.rb
+++ app/models/data_category.rb
@@ -1,6 +1,6 @@
 class DataCategory < ApplicationRecord
-  has_many :service_data_categories
-  has_many :schema_data_categories
+  has_many :service_data_categories, dependent: :destroy
+  has_many :schema_data_categories, dependent: :destroy
   has_many :services, through: :service_data_categories
   has_many :schemas, through: :schema_data_categories
 end
