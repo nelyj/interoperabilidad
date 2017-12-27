97869a5b87305c048e0766e283c13db23ae6017a
afiebig <alfredo.fiebig@continuum.cl>
2017-12-27 10:06:04 -0300

    Add test for inject agreement without one of the params

--- test/features/agreement_injection_test.rb
+++ test/features/agreement_injection_test.rb
@@ -25,8 +25,7 @@ class AgreementFlowTest < Capybara::Rails::TestCase
 
   end
 
-
-  test 'GobDigital User can inject new Agreement' do
+  test 'GobDigital User can not inject new Agreement with same Organization' do
     login_as users(:pablito), scope: :user
     visit root_path
 
@@ -36,9 +35,107 @@ class AgreementFlowTest < Capybara::Rails::TestCase
     end
 
     assert_content('Vista Global de Convenios')
-    assert_link ('Subir un convenio existente')
-    assert find(:xpath, '//table/thead/tr').text.include?('Fecha de creación Institución proveedora Servicios Institución solicitante Estado Responsables Última modificación Fecha de Firma')
+    find_link('Subir un convenio existente').click
 
+    assert_content('Crear Nuevo Convenio')
+
+    page.execute_script('$("input[type=file]").show()')
+    attach_file '_agreements_global_file', Rails.root.join('test/files/sample-pdf/content.pdf')
+
+    find("#select2-_agreements_global_service_consumer_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    find("#select2-agreement_service_provider_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    click_button 'Crear Nuevo Convenio'
+
+    assert_content("No se pudo crear el convenio")
+    assert_content("Las organizaciones deben ser distintas")
+  end
+
+  test 'GobDigital User can not inject new Agreement without a PDF' do
+    login_as users(:pablito), scope: :user
+    visit root_path
+
+    find('#user-menu').click
+    within '#user-menu' do
+      find('#menu-vista-global-convenios').click
+    end
+
+    assert_content('Vista Global de Convenios')
+    find_link('Subir un convenio existente').click
+
+    assert_content('Crear Nuevo Convenio')
+
+    find("#select2-_agreements_global_service_consumer_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    find("#select2-agreement_service_provider_organization_id-container").click
+    find_all(".select2-results__option")[2].click
+
+    click_button 'Crear Nuevo Convenio'
+
+    assert_content("No se pudo crear el convenio")
+    assert_content("El archivo debe ser un pdf")
+  end
+
+  test 'GobDigital User can not inject new Agreement with a file extension different from PDF' do
+    login_as users(:pablito), scope: :user
+    visit root_path
+
+    find('#user-menu').click
+    within '#user-menu' do
+      find('#menu-vista-global-convenios').click
+    end
+
+    assert_content('Vista Global de Convenios')
+    find_link('Subir un convenio existente').click
+
+    assert_content('Crear Nuevo Convenio')
+
+    page.execute_script('$("input[type=file]").show()')
+    attach_file '_agreements_global_file', Rails.root.join('test/files/sample-pdf/content.txt')
+
+    find("#select2-_agreements_global_service_consumer_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    find("#select2-agreement_service_provider_organization_id-container").click
+    find_all(".select2-results__option")[2].click
+
+    click_button 'Crear Nuevo Convenio'
+
+    assert_content("No se pudo crear el convenio")
+    assert_content("El archivo debe ser un pdf")
+  end
+
+  test 'GobDigital User can not inject new Agreement without services' do
+    login_as users(:pablito), scope: :user
+    visit root_path
+
+    find('#user-menu').click
+    within '#user-menu' do
+      find('#menu-vista-global-convenios').click
+    end
+
+    assert_content('Vista Global de Convenios')
+    find_link('Subir un convenio existente').click
+
+    assert_content('Crear Nuevo Convenio')
+
+    page.execute_script('$("input[type=file]").show()')
+    attach_file '_agreements_global_file', Rails.root.join('test/files/sample-pdf/content.pdf')
+
+    find("#select2-_agreements_global_service_consumer_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    find("#select2-agreement_service_provider_organization_id-container").click
+    find_all(".select2-results__option")[2].click
+
+    click_button 'Crear Nuevo Convenio'
+
+    assert_content("No se pudo crear el convenio")
+    assert_content("Services no puede estar en blanco")
   end
 
 end
