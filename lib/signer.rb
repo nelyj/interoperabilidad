a1b0151ab1fd5f7c29c683bd65bd7562c583ac72
afiebig <alfredo.fiebig@continuum.cl>
2017-12-28 16:04:56 -0300

    use real data, not the one in the examples

--- lib/signer.rb
+++ lib/signer.rb
@@ -16,9 +16,9 @@ class SignerApi
 
     payload = {
       expiration: Time.now.utc.iso8601[0...-1], #Had to remove the Z from the UTC time, because the api does not suport it.
-      run: "1111111",#user.rut_number,
-      purpose: 'Propósito General', #"Propósito General", to use OTP on file sign.
-      entity: "Subsecretaría General de La Presidencia" #organization.name
+      run: user.rut_number, #"1111111"
+      purpose: 'Propósito General', # Use OTP on file sign.
+      entity: organization.name #"Subsecretaría General de La Presidencia"
     }
 
     token = SignerApi.encode_token(payload)
@@ -36,12 +36,13 @@ class SignerApi
 
     data = {files: files, token: token, api_token_key: api_token_key}
 
-    puts data.to_json
-
     begin
       response = RestClient.post( url + endpoint,
         data.to_json, :content_type => :json, :accept => :json, Host: URI.parse(url).host)
+      puts response.to_json
+      response
     rescue => e
+      puts e.response.to_json
       Rollbar.error('Upload file to Signer Service URL: ' + url +
        ' path: ' + endpoint + ' user: ' + user.name + ' organization: ' + organization.name + ' returned: ' + e.response)
       return e.response
@@ -54,7 +55,10 @@ class SignerApi
 
     begin
       response = RestClient.get(url + endpoint + sesion_token, OTP: otp)
+      puts response.to_json
+      response
     rescue => e
+      puts e.response.to_json
       Rollbar.error('Sign document in Signer Service URL: ' + url +
         'path: ' + endpoint + ' sesion_token: ' + sesion_token + ' returned: ' + e.response)
       return e.response
