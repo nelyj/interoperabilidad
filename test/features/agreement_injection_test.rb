62e4f7b7dc451bc0ad5f384a5705e97abc0fd1ed
afiebig <alfredo.fiebig@continuum.cl>
2017-12-29 11:09:49 -0300

    Add Test for Agreement Injection

--- test/features/agreement_injection_test.rb
+++ test/features/agreement_injection_test.rb
@@ -138,4 +138,36 @@ class AgreementInjectionTest < Capybara::Rails::TestCase
     assert_content("Services no puede estar en blanco")
   end
 
+  test 'GobDigital User can inject new Agreement' do
+    login_as users(:pablito), scope: :user
+    visit root_path
+
+    find('#user-menu').click
+    within '#user-menu' do
+      find('#menu-vista-global-convenios').click
+    end
+
+    assert_content('Vista Global de Convenios')
+    find_link('Subir un convenio existente').click
+
+    assert_content('Crear Nuevo Convenio')
+
+    page.execute_script('$("input[type=file]").show()')
+    attach_file '_agreements_global_file', Rails.root.join('test/files/sample-pdf/content.pdf')
+
+    find("#select2-_agreements_global_service_consumer_organization_id-container").click
+    find_all(".select2-results__option")[1].click
+
+    find("#select2-agreement_service_provider_organization_id-container").click
+    find_all(".select2-results__option")[3].click
+
+    page.execute_script('$("input[type=checkbox]").show()')
+
+    assert_content('servicio_2')
+    page.find('label', text: 'servicio_2').click
+
+    click_button 'Crear Nuevo Convenio'
+    assert_content('Convenio creado correctamente')
+  end
+
 end
